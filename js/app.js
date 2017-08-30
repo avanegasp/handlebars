b2e17fb1fc03aa6a13c345579c1f3c8e66c1b016
avanegasp <arco527@gmail.com>
2017-08-30 16:38:42 -0500

    helper

--- js/app.js
+++ js/app.js
@@ -1,6 +1,23 @@
 var source = $('#entry-template').html();
 var template = Handlebars.compile(source);
 
+Handlebars.registerHelper('enumeracion', function(pvalor, operador, svalor){
+
+  pvalor = parseFloat(pvalor);
+  svalor = parseFloat(svalor);
+
+  var operadores={
+    '+' : pvalor + svalor,
+    '*' : pvalor * svalor,
+    '-' : pvalor - svalor
+  }
+
+  return operadores[operador]
+
+})
+
+
+
 var personas = [
               {'nombre': 'Angie','apellido': 'Vanegas','correo': 'arco527@gmail.com'},
               {'nombre': 'María','apellido': 'Casas','correo': 'lilo@gmail.com'},
