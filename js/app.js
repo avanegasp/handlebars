ecb53b2c9d5fe677dfa5384a9df8c49c21e9ab68
avanegasp <arco527@gmail.com>
2017-08-30 15:41:56 -0500

    Several names

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
+var source = $('#entry-template').html();
+var template = Handlebars.compile(source);
 
+var personas = [
+              {'nombre': 'Angie','apellido': 'Vanegas','correo': 'arco527@gmail.com'},
+              {'nombre': 'María','apellido': 'Casas','correo': 'lilo@gmail.com'},
+              {'nombre': 'Pedro','apellido': 'Ramirez','correo': 'pepe655@gmail.com'},
+            ];
+
+var html= template({'persona': personas});
+$('.container').html(html);
